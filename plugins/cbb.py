cb36e6dbff93d0b94364517b42b81f6e42f2f16f
songokuop <149889315+songokuop@users.noreply.github.com>
2024-02-04 08:32:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>😎 Creator : <a href='https://t.me/Movies_Zone_Media'>Mᴏᴠɪᴇs Zᴏɴᴇ</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n🌚 Source Code : <a href='https://github.com'>Nᴏᴛ Aᴠᴀɪʟᴀʙʟᴇ</a>\n😎 Channel : @Movies_Zone_Media\n😊 Support Group : @Movies_Zone_Support</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
